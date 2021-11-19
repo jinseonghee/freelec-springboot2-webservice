1dc384367e8236f1623dec2048b7eef387aea5db
jinseonghee <wlstjdgml0610@gmail.com>
2021-11-19 22:41:02 +0900

    feat: update posts

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
@@ -2,11 +2,11 @@ package com.jojoldu.book.freelecspringboot2webservice.service.posts;
 
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.Posts;
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.PostsRepository;
+import com.jojoldu.book.freelecspringboot2webservice.web.PostsResponseDto;
+import com.jojoldu.book.freelecspringboot2webservice.web.PostsUpdateRequestDto;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 import javax.transaction.Transactional;
 
@@ -19,5 +19,16 @@ public class PostsService {
     @Transactional // 데이터 베이스의 상태를 변경하는 작업이나 한번에 수행되어야 하는 연산들을 의미, 예외 발생시 자동으로 rollback 처리
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+
+    }
+
+    public PostsResponseDto findById(Long id) {
     }
 }
