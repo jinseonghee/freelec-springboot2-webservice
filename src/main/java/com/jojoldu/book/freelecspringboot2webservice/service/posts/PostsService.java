ba0f58b6cdc4ee1709de197d8a4974e05714b606
jinseonghee <wlstjdgml0610@gmail.com>
2021-12-01 23:01:22 +0900

    feat: Implement a function of the delete button

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
@@ -50,4 +50,13 @@ public class PostsService {
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts); //엔티티를 파라미터로 삭제할 수도 있고 deleteById 메소드를 이용하면 id로 삭제할 수 있다.
+                                       //존재하는 Posts인지 확인을 위해 엔티티 조회 후 그대로 삭제
+    }
 }
