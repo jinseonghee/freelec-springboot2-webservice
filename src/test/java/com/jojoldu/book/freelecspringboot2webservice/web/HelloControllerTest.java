a64a56bc274eb39156ff9a494cab59796dd83fee
jinseonghee <wlstjdgml0610@gmail.com>
2021-12-30 03:34:44 +0900

    test: improve test coverage
    test: improve test coverage

--- src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloControllerTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -15,15 +18,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.jojoldu.book.freelecspringboot2webservice.config.auth.SecurityConfig;
+
 @RunWith(SpringRunner.class)
 //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴. 여기선 SpringRunner 라는 스프링 실행자를 사용.(스프링 부트 테스트와 JUnit 사이의 연결자 역할)
-@WebMvcTest(controllers = HelloController.class) //여러 스프링 어노테이션 중, web(spring MVC)에 집중할 수 있는 어노테이션
-//@Controller, @ControllerAdvice 등 사용 가능(@Service, @component, @Repository 등 은 사용 불가)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = { //여러 스프링 어노테이션 중, web(spring MVC)에 집중할 수 있는 어노테이션
+                        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)}) //@Controller, @ControllerAdvice 등 사용 가능(@Service, @component, @Repository 등 은 사용 불가)
 public class HelloControllerTest {
 
     @Autowired //스프링이 관리하는 bean을 주입받음
     private MockMvc mvc; //웹 API를 테스트 할 떄 사용, 스프링 MVC 테스트의 시작점.(이 클래스를 통해 HTTP, GET, POST등에 대한 API 테스트를 할 수 있다.)
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -33,6 +39,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); //응답 본문의 내용을 검증.
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
--- src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/PostApiControllerTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/PostApiControllerTest.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.freelecspringboot2webservice.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.Posts;
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.PostsRepository;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
@@ -18,7 +19,9 @@ import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,6 +31,9 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // RANDOM_PORT 실행
@@ -37,11 +43,24 @@ public class PostApiControllerTest {
     @LocalServerPort
     private int port;
 
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Autowired
     private PostsRepository postsRepository;
+
+    @Before // 매번 테스트가 시작되기 전에 MockMvc 인스턴스 생성
+    public void setup() {
+        mvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
 
     @After
     public void tearDown() throws Exception {
@@ -49,6 +68,7 @@ public class PostApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER") //인증된 모의 사용자를 만들어서 사용. roles에 권한 추가 가능.
     public void Posts_등록된다() throws Exception {
 
         //given
@@ -64,11 +84,16 @@ public class PostApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        //ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        mvc.perform(post(url) //생성된 MockMvc를 통해 API를 테스트.
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content(new ObjectMapper().writeValueAsString(requestDto))) //본문(Body) 영역은 문자열로 표현하기 위해 ObjectMapper를 통해 json으로 반환
+            .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        //assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
@@ -77,6 +102,7 @@ public class PostApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER") // 이 어노테이션으로 인해 ROLE_USER 권한을 가진 사용자가 API를 요청하는 것과 동일한 효과를 가지게 된다.
     public void Posts_수정된다() throws Exception {
 
         //given
@@ -100,11 +126,16 @@ public class PostApiControllerTest {
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        //ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        //assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
